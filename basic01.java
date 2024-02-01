80771a3fdfba811338ad4e4ceb05b2f8403c61fd
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-01 21:36:33 +0530

    CH01BASIC

---
+++ basic01.java
@@ -0,0 +1,9 @@
+// public class basic01{
+//     public static void main(String args[]){
+//         System.out.print("Vishal Maur");
+//         System.out.println("Vishal Maur");
+//         System.out.print("Vishal Maur\n");
+//         System.out.print("Vishal Maur");
+//     }
+
+// }

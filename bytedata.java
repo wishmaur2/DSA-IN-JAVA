b5caeeb179e59f3a9512462804fa8a0f6cd3e041
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-02 10:56:58 +0530

    CH02Variable&Datatypes

---
+++ bytedata.java
@@ -0,0 +1,12 @@
+public class bytedata {
+    public static void main (String args[]){
+        byte a=126;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        // long b;
+        // System.out.println(b);
+    }
+}

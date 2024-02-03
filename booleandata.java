3d83fb75202ca8d6190c91adbdcf9120e218c4bb
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-03 10:59:33 +0530

    CH02Variable&Datatypes
    CH02Variable&Datatypes

---
+++ booleandata.java
@@ -0,0 +1,16 @@
+public class booleandata{
+    public static void main(String args[]){
+        // boolean a1= false;
+        // boolean a2=true;
+        int a=10;
+        int b=20;
+        int c=10;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a==c);
+        // System.out.println(a==a);
+        System.out.println(a<=b);
+
+
+    }
+}

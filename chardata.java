a737aaa6324808bc17497656cae0721261cf3a3c
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-04 11:13:12 +0530

    CH02Variable&Datatypes
    CH02Variable&Datatypes

---
+++ chardata.java
@@ -0,0 +1,15 @@
+public class chardata {
+    public static void main (String args[]){
+        char ch='b';
+        System.out.println(ch);
+        char ch1=65;
+        char ch2=66;
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+        char ch3=97;
+        System.out.println(ch3);
+
+
+    }
+}

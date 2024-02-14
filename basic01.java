fd3f02426da98c62ff3923e945bc776eb1adc96a
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-14 10:32:34 +0530

    CH11String
    CH11String

--- basic01.java
+++ basic01.java
@@ -1,9 +1,48 @@
-// public class basic01{
-//     public static void main(String args[]){
-//         System.out.print("Vishal Maur");
-//         System.out.println("Vishal Maur");
-//         System.out.print("Vishal Maur\n");
-//         System.out.print("Vishal Maur");
-//     }
+// import java.util.*;
+//String is IMMUTABLE -->not changeble
+public class basic01{
 
-// }
+    public static void main (String args[]){
+        // char c='a';
+        // char arr[]={'a','b','c'};
+        
+     //Declare
+        // String str="Wish";
+        // String str2=new String("Wish");
+
+        // System.out.println(str);
+        // System.out.println(str2);
+
+     // INPUT & OUTPUT
+        // Scanner sc=new Scanner(System.in);
+        // // String name=sc.next();
+        // String name2=sc.nextLine();
+        // // System.out.println(name);
+        // System.out.println(name2);
+        // sc.close();
+    
+     //String Length --> In string length is a function , not only property 
+        // System.out.println(name2.length());
+
+     //String Concatenation
+        String fname="Wish";
+        String lname="Maur";
+        String Fname=fname+" "+lname;
+        System.out.println(Fname);
+
+     //charAt method call
+         printletter(Fname);  
+
+
+    }
+    
+
+    //charAt method
+    public static void printletter(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+        System.out.println();
+    }
+
+}

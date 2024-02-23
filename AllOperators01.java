91f3bd921280105ce527c58fb8cec0c182a2b38f
VISHAL KUMAR MAURYA <141044943+wishmaur2@users.noreply.github.com>
2024-02-23 22:07:07 +0530

    CH12BitManipulation
    CH12BitManipulation

---
+++ AllOperators01.java
@@ -0,0 +1,13 @@
+public class AllOperators01 {
+    public static void main (String args[]){
+
+        System.out.println(4&6);
+        System.out.println(4|6);
+        System.out.println(4^6);
+        System.out.println(~4);
+        System.out.println(~0);
+        System.out.println(5<<2);
+        System.out.println(5>>2);
+    }
+    
+}
